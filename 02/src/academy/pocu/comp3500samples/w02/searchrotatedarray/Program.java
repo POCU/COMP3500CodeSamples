21ac838e7e057fb6ab585efe817b2f55f6e5c834
monkeywithbanana <>
2020-08-29 17:36:18 -0700

    w02 refactoring

--- 02/src/academy/pocu/comp3500samples/w02/searchrotatedarray/Program.java
+++ 02/src/academy/pocu/comp3500samples/w02/searchrotatedarray/Program.java
@@ -5,46 +5,46 @@ public class Program {
     public static void main(String[] args) {
         int[] arry = new int[]{20, 25, 26, 29, 33, 1, 3, 5, 6, 10, 11, 19};
 
-        int index = indexOfRotatedArray(arry, 0, 0, arry.length - 1);
+        int index = indexOfRotatedArray(arry, 0, arry.length - 1, 0);
 
         System.out.println(index);
 
-        index = indexOfRotatedArray(arry, 1, 0, arry.length - 1);
+        index = indexOfRotatedArray(arry, 0, arry.length - 1, 1);
 
         System.out.println(index);
 
-        index = indexOfRotatedArray(arry, 29, 0, arry.length - 1);
+        index = indexOfRotatedArray(arry, 0, arry.length - 1, 29);
 
         System.out.println(index);
 
-        index = indexOfRotatedArray(arry, 6, 0, arry.length - 1);
+        index = indexOfRotatedArray(arry, 0, arry.length - 1, 6);
 
         System.out.println(index);
     }
 
-    private static int indexOfRotatedArray(int[] arry, int target, int start, int end) {
+    private static int indexOfRotatedArray(int[] arry, int start, int end, int num) {
         if (start > end) {
             return -1;
         }
 
         int mid = (start + end) / 2;
 
-        if (arry[mid] == target) {
+        if (arry[mid] == num) {
             return mid;
         }
 
         if (arry[start] <= arry[mid]) {
-            if (target >= arry[start] && target <= arry[mid]) {
-                return indexOfRotatedArray(arry, target, start, mid - 1);
+            if (num >= arry[start] && num <= arry[mid]) {
+                return indexOfRotatedArray(arry, start, mid - 1, num);
             }
 
-            return indexOfRotatedArray(arry, target, mid + 1, end);
+            return indexOfRotatedArray(arry, mid + 1, end, num);
         }
 
-        if (target >= arry[mid] && target <= arry[end]) {
-            return indexOfRotatedArray(arry, target, mid + 1, end);
+        if (num >= arry[mid] && num <= arry[end]) {
+            return indexOfRotatedArray(arry, mid + 1, end, num);
         }
 
-        return indexOfRotatedArray(arry, target, start, mid - 1);
+        return indexOfRotatedArray(arry, start, mid - 1, num);
     }
 }
--- 02/src/academy/pocu/comp3500samples/w02/sumtailrecursive/Program.java
+++ 02/src/academy/pocu/comp3500samples/w02/sumtailrecursive/Program.java
@@ -2,32 +2,28 @@ package academy.pocu.comp3500samples.w02.sumtailrecursive;
 
 public class Program {
     public static void main(String[] args) {
-        int sum = sumTailRecursive(10);
+        int sum = sumTailRecursive(10, 0);
 
         System.out.println(sum); // 55
 
-        sum = sumTailRecursive(100);
+        sum = sumTailRecursive(100, 0);
 
         System.out.println(sum); // 5050
 
-        sum = sumTailRecursive(1000);
+        sum = sumTailRecursive(1000, 0);
 
         System.out.println(sum); // 500500
 
-        sum = sumTailRecursive(100000); // ??
+        sum = sumTailRecursive(100000, 0); // ??
 
         System.out.println(sum);
     }
 
-    private static int sumTailRecursive(int n) {
-        return sumTailRecursiveHelper(n, 0);
-    }
-
-    private static int sumTailRecursiveHelper(int n, int sum) {
+    private static int sumTailRecursive(int n, int sum) {
         if (n <= 0) {
             return sum;
         }
 
-        return sumTailRecursiveHelper(n - 1, sum + n);
+        return sumTailRecursive(n - 1, sum + n);
     }
 }
