b9d3c2f87a6258302a6c686515bb6402b0ba5c93
John Choi <john.choi@pocu.academy>
2021-10-23 16:05:28 -0700

    [w02] renamed number array variables and parameters

--- 02/src/academy/pocu/comp3500samples/w02/searchrotatedarray/Program.java
+++ 02/src/academy/pocu/comp3500samples/w02/searchrotatedarray/Program.java
@@ -3,48 +3,48 @@ package academy.pocu.comp3500samples.w02.searchrotatedarray;
 public class Program {
 
     public static void main(String[] args) {
-        int[] arry = new int[]{20, 25, 26, 29, 33, 1, 3, 5, 6, 10, 11, 19};
+        int[] numbers = new int[]{20, 25, 26, 29, 33, 1, 3, 5, 6, 10, 11, 19};
 
-        int index = indexOfRotatedArray(arry, 0, arry.length - 1, 0);
-
-        System.out.println(index);
-
-        index = indexOfRotatedArray(arry, 0, arry.length - 1, 1);
+        int index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 0);
 
         System.out.println(index);
 
-        index = indexOfRotatedArray(arry, 0, arry.length - 1, 29);
+        index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 1);
 
         System.out.println(index);
 
-        index = indexOfRotatedArray(arry, 0, arry.length - 1, 6);
+        index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 29);
+
+        System.out.println(index);
+
+        index = indexOfRotatedArray(numbers, 0, numbers.length - 1, 6);
 
         System.out.println(index);
     }
 
-    private static int indexOfRotatedArray(int[] arry, int start, int end, int num) {
+    private static int indexOfRotatedArray(int[] numbers, int start, int end, int num) {
         if (start > end) {
             return -1;
         }
 
         int mid = (start + end) / 2;
 
-        if (arry[mid] == num) {
+        if (numbers[mid] == num) {
             return mid;
         }
 
-        if (arry[start] <= arry[mid]) {
-            if (num >= arry[start] && num <= arry[mid]) {
-                return indexOfRotatedArray(arry, start, mid - 1, num);
+        if (numbers[start] <= numbers[mid]) {
+            if (num >= numbers[start] && num <= numbers[mid]) {
+                return indexOfRotatedArray(numbers, start, mid - 1, num);
             }
 
-            return indexOfRotatedArray(arry, mid + 1, end, num);
+            return indexOfRotatedArray(numbers, mid + 1, end, num);
         }
 
-        if (num >= arry[mid] && num <= arry[end]) {
-            return indexOfRotatedArray(arry, mid + 1, end, num);
+        if (num >= numbers[mid] && num <= numbers[end]) {
+            return indexOfRotatedArray(numbers, mid + 1, end, num);
         }
 
-        return indexOfRotatedArray(arry, start, mid - 1, num);
+        return indexOfRotatedArray(numbers, start, mid - 1, num);
     }
 }
